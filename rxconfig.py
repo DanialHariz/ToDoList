9c7f0a89deb987d851d3facbcdfd626cc2464a23
danialhariz <danialhariz.mdaud@gmail.com>
2025-07-21 14:06:42 +0800

    Fix: bind to Cloud Run port from environment variable

--- rxconfig.py
+++ rxconfig.py
@@ -1,6 +1,11 @@
 import reflex as rx
+import os
 
 config = rx.Config(
-    app_name="ToDoList",  # This should match the folder 'todo' that contains app.py
-    
+    app_name="ToDoList",
+    port=int(os.environ.get("PORT", 3000))  # default to 3000 if not set
 )
+# config = rx.Config(
+#     app_name="ToDoList",  # This should match the folder 'todo' that contains app.py
+    
+# )
