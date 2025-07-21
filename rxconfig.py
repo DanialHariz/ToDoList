f511c6f3f09bb4fc420e88c80a83a72284327141
danialhariz <danialhariz.mdaud@gmail.com>
2025-07-21 19:39:27 +0800

    Fix: rxconfig port to 8080

--- rxconfig.py
+++ rxconfig.py
@@ -3,7 +3,7 @@ import os
 
 config = rx.Config(
     app_name="ToDoList",
-    port=int(os.environ.get("PORT", 3000))  # default to 3000 if not set
+    port=int(os.environ.get("PORT", 8080))
 )
 # config = rx.Config(
 #     app_name="ToDoList",  # This should match the folder 'todo' that contains app.py
